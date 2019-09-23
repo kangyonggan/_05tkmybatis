d8e643697f0e39a2c8be029f7640222cb05e3d86
kangyonggan <kangyonggan@gmail.com>
2019-09-23 12:17:42 +0800

    使用@MapperScan

--- src/main/java/com/kangyonggan/demo/Application.java
+++ src/main/java/com/kangyonggan/demo/Application.java
@@ -2,12 +2,14 @@ package com.kangyonggan.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import tk.mybatis.spring.annotation.MapperScan;
 
 /**
  * @author kangyonggan
  * @since 2019-09-19
  */
 @SpringBootApplication
+@MapperScan("com.kangyonggan.demo.mapper")
 public class Application {
 
     public static void main(String[] args) {
--- src/main/java/com/kangyonggan/demo/mapper/UserMapper.java
+++ src/main/java/com/kangyonggan/demo/mapper/UserMapper.java
@@ -1,13 +1,12 @@
 package com.kangyonggan.demo.mapper;
 
+import com.kangyonggan.demo.BaseMapper;
 import com.kangyonggan.demo.User;
-import org.apache.ibatis.annotations.Mapper;
 
 /**
  * @author kangyonggan
  * @since 2019-09-23
  */
-@Mapper
 public interface UserMapper extends BaseMapper<User> {
 
 }
